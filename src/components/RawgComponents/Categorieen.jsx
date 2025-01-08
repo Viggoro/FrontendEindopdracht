be2905c6e25f75b925b42933854607bb96f42706
Viggoro <viggo_rosendahl@hotmail.nl>
2025-01-08 20:38:51 +0100

    Screenshots op gamedetails pagina toegevoegd

---
+++ src/components/RawgComponents/Categorieen.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Categoerieen extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    ‎ ‎ Categorieën‎ ‎ ‎
+                </h2>
+                <p>
+                    Nieuw
+                </p>
+                <p>
+                    Populair
+                </p>
+                <p>
+                    Gratis
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Categoerieen;
